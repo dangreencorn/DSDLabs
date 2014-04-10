7118fbb2aa153652486082819e98823cd9513870
unknown <mcarru@156TR4060DS-06.campus.MCGILL.CA>
2014-04-09 22:12:27 -0400

    to_utc and to_local now work

---
+++ Lab5/g01_YMD_Counter.vhd
@@ -0,0 +1,112 @@
+-- entity name: g01_YMD_Counter
+--
+-- Counts up in years, months, and days. Can be arbitrarily set to any date or reset to 0001-01-01.
+-- This circuit does take leap years into account.
+--
+-- Copyright (C) 2014 Alex Carruthers, Dan Grencorn 
+-- Version 1.0
+-- Author: Alex Carruthers, Dan Greencorn; michael.carruthers@mail.mcgill.ca, dan.greencorn@mail.mcgill.ca 
+-- Date: March 27, 2014
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity g01_YMD_Counter is
+	port ( 
+		clock : in std_logic;
+		reset : in std_logic;
+		day_count_en : in std_logic;
+		load_en : in std_logic;
+		Y_Set : in std_logic_vector(11 downto 0);
+		M_Set : in std_logic_vector(3 downto 0);
+		D_Set : in std_logic_vector(4 downto 0);
+		Years : out std_logic_vector(11 downto 0);
+		Months : out std_logic_vector(3 downto 0);
+		Days : out std_logic_vector(4 downto 0)
+	);
+end g01_YMD_Counter;
+
+
+architecture behaviour of g01_YMD_Counter is
+	signal int_years : integer range 1 to (2**Years'length)-1 := 1;
+	signal int_months : integer range 1 to (2**Months'length)-1 := 1;
+	signal int_days : integer range 1 to (2**Days'length)-1 := 1;
+	signal last_day_of_month : std_logic := '0';
+	signal leap_year : std_logic := '0';
+	signal year_count_en : std_logic := '0';
+	signal month_count_en : std_logic := '0';
+begin
+	is_leap_year : process(int_years)
+	begin
+		if (int_years mod 4 /= 0) then leap_year <='0';
+		elsif (int_years mod 100 /= 0) then leap_year <='1';
+		elsif (int_years mod 400 = 0) then leap_year <='1';
+		else leap_year <='0';
+		end if;
+	end process;
+	
+	is_last_day_of_month : process(int_days)
+	begin
+		if ((int_days = 30 AND (int_months = 4 OR int_months = 6 OR int_months = 9 OR int_months = 11))
+				OR
+				(int_days = 31 AND (int_months = 1 OR int_months = 3 OR int_months = 5 OR int_months = 7 OR int_months = 8 OR int_months = 10 OR int_months = 12))
+				OR
+				(int_days = 28 AND int_months = 2 AND leap_year = '0')
+				OR
+				(int_days = 29 AND int_months = 2 AND leap_year = '1')) then
+			last_day_of_month <= '1';
+		else last_day_of_month <= '0';
+		end if;
+	end process;
+	
+	days_counter : process(clock, reset)
+	begin
+		if (reset = '1') then int_days <= 1;
+		elsif (clock'event and clock = '1') then
+			if (load_en = '1') then int_days <= to_integer(unsigned(D_Set));
+			elsif (day_count_en = '1') then
+				if (last_day_of_month = '1') then int_days <= 1;
+				else int_days <= int_days + 1;
+				end if;
+			end if;
+			if (last_day_of_month = '1') then month_count_en <= '1';
+			else month_count_en <= '0';
+			end if;
+		end if;
+	end process;
+	
+	months_counter : process(clock, reset)
+	begin
+		if (reset = '1') then int_months <= 1;
+		elsif (clock'event and clock = '1') then
+			if (load_en = '1') then int_months <= to_integer(unsigned(M_Set));
+			elsif (month_count_en = '1' and day_count_en = '1') then
+				if (int_months = 12) then int_months <= 1;
+				elsif (day_count_en = '1') then int_months <= int_months + 1;
+				end if;
+			end if;
+			if (int_months = 12 and last_day_of_month = '1') then year_count_en <= '1';
+			else year_count_en <= '0';
+			end if;
+		end if;
+	end process;
+	
+	years_counter : process(clock, reset)
+	begin
+		if (reset = '1') then int_years <= 1;
+		elsif (clock'event and clock = '1') then
+			if (load_en = '1') then int_years <= to_integer(unsigned(Y_Set));
+			elsif (year_count_en = '1' and day_count_en = '1') then
+				if (day_count_en = '1') then int_years <= int_years + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	Years <= std_logic_vector(to_unsigned(int_years,12));
+	Months <= std_logic_vector(to_unsigned(int_months,4));
+	Days <= std_logic_vector(to_unsigned(int_days,5));
+end behaviour;
+
---
+++ Lab5/g01_to_local.vhd
@@ -0,0 +1,126 @@
+-- entity name: g01_to_local 
+--
+-- Copyright (C) 2014 Alex Carruthers, Dan Grencorn 
+-- Version 1.0
+-- Author: Alex Carruthers, Dan Greencorn; michael.carruthers@mail.mcgill.ca, dan.greencorn@mail.mcgill.ca 
+-- Date: April 9, 2014
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity g01_to_local is
+	port ( 
+		Year_in : in std_logic_vector(11 downto 0);
+		Month_in : in std_logic_vector(3 downto 0);
+		Day_in : in std_logic_vector(4 downto 0);
+		Hour_in : in std_logic_vector(4 downto 0);
+		Minute_in : in std_logic_vector(5 downto 0);
+		Second_in : in std_logic_vector(5 downto 0);
+		
+		DST : in std_logic;
+		Time_Zone : in std_logic_vector(4 downto 0);
+		
+		Year_out : out std_logic_vector(11 downto 0);
+		Month_out : out std_logic_vector(3 downto 0);
+		Day_out : out std_logic_vector(4 downto 0);
+		Hour_out : out std_logic_vector(4 downto 0);
+		Minute_out : out std_logic_vector(5 downto 0);
+		Second_out : out std_logic_vector(5 downto 0)
+	);
+end g01_to_local;
+
+architecture behaviour of g01_to_local is
+	signal Month_in_int : integer range 1 to 12;
+	signal leap_year : std_logic;
+begin
+	-- Minutes and seconds aren't affected so we'll just pass them through to the output
+	Minute_out <= Minute_in;
+	Second_out <= Second_in;
+
+	calculate_hour : process(Year_in, Month_in, Day_in, Hour_in, DST, Time_Zone, leap_year)
+		variable year : integer := to_integer(unsigned(Year_in));
+		variable month : integer := to_integer(unsigned(Month_in));
+		variable day : integer := to_integer(unsigned(Day_in));
+		variable hour : integer := to_integer(unsigned(Hour_in));
+		variable orig_month : integer := to_integer(unsigned(Month_in));
+	begin
+		year := to_integer(unsigned(Year_in));
+		month := to_integer(unsigned(Month_in));
+		day := to_integer(unsigned(Day_in));
+		hour := to_integer(unsigned(Hour_in));
+		orig_month := to_integer(unsigned(Month_in));
+		
+		if (year mod 4 /= 0) then leap_year <='0';
+		elsif (year mod 100 /= 0) then leap_year <='1';
+		elsif (year mod 400 = 0) then leap_year <='1';
+		else leap_year <='0';
+		end if;
+				
+		if (DST = '1') then 
+			hour := hour + 1;
+		end if;
+		
+		hour := hour + (to_integer(unsigned(Time_Zone)) - 12);
+		
+		if (hour < 0) then
+			day := day - 1;
+			hour := 24 + hour;
+			if (day = 0) then
+				month := month - 1;
+				
+				if (orig_month = 1 
+						OR orig_month = 2 
+						OR orig_month = 4 
+						OR orig_month = 6 
+						OR orig_month = 9 
+						OR orig_month = 11) then
+					day := 31;
+				elsif (orig_month = 5 
+						OR orig_month = 5 
+						OR orig_month = 10 
+						OR orig_month = 12) then
+					day := 30;
+				elsif (orig_month = 3 AND leap_year = '1') then
+					day := 29;
+				elsif (orig_month = 3 AND leap_year = '0') then
+					day := 28;
+				end if;
+				
+				if (month = 0) then
+					month := 12;
+					year := year - 1;
+				end if;
+			end if;
+		elsif (hour > 23) then
+			day := day + 1;
+			hour := 24 - hour;
+			
+			if ( ( day = 32 AND ( orig_month = 1
+								  OR orig_month = 3
+								  OR orig_month = 5
+								  OR orig_month = 7
+								  OR orig_month = 8
+								  OR orig_month = 10
+								  OR orig_month = 12 ) )
+					OR ( day = 31 AND ( orig_month = 4
+										OR orig_month = 6
+										OR orig_month = 9
+										OR orig_month = 11 ) )
+					OR ( day = 30 AND orig_month = 2 AND leap_year = '1')
+					OR ( day = 30 AND orig_month = 2 AND leap_year = '0')) then
+				month := month + 1;
+				day := 1;
+				if (month = 13) then
+					year := year + 1;
+					month := 1;
+				end if;
+			end if;
+		end if;
+		Year_out <= std_logic_vector(to_unsigned(year, 12));
+		Month_out <= std_logic_vector(to_unsigned(month, 4));
+		Day_out <= std_logic_vector(to_unsigned(day, 5));
+		Hour_out <= std_logic_vector(to_unsigned(hour, 5));
+		
+	end process;
+end behaviour;
